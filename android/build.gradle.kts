b1f1994a7f866ba1d337b6713bb31c2e2753bdab
Kawsar Ali <59552845+Kawsar07@users.noreply.github.com>
2025-11-03 15:47:25 +0600

    updated folder name

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -16,7 +16,7 @@ subprojects {
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 subprojects {
-    project.evaluationDependsOn(":app")
+    project.evaluationDependsOn(":apk")
 }
 
 tasks.register<Delete>("clean") {
